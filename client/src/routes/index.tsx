1f8db1f91b8a7fd6fce874891cb4d10089102a69
wilhamsJW <wilhams.dev@gmail.com>
2022-01-21 13:51:40 -0300

    feat: adding react-router-dom

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,1 +1,16 @@
-export {  };
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+export const AppRoutes = () => {
+
+    return (
+        <Routes>
+            <Route path='/page-initial' element={<p>Page initial</p>} />
+
+            <Route path='*' element={<Navigate to="/page-initial" />} /> 
+            {/** Navigate serve como redirect, o redirect foi descontinuado em versões mais recentes
+             * caso o user digite uma rota errada na url ele será redirecionado para a página principal graças ao navigate
+             * Navigate irá olhar para todas as rotas acima dele e verá q não existe tal rota e redirecionará pra a prinipal
+             */}
+        </Routes>
+    );
+};
